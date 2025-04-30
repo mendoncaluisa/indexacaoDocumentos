05bac4884f63157e449a0e5aea4fe4ade7ec26ea
Ivan Marcelino <ivanmarcelino2004@gmail.com>
2025-04-30 00:12:44 -0300

    Adição de verificação de palavra

--- projeto.py
+++ projeto.py
@@ -7,29 +7,43 @@
 # excluir palavras proibidas na indexação ex: não, separadas por hifen, tamanho menor que 2
 
 # contabilizando numero de repetição de cada palavra
-import sys
 import re
 import os
 
+
 def indexacao():
-    dir = "pasta"
+    dir = "./documentos"
     words = dict()
 
+    # itera por todos nomes de arquivos na pasta
     for nome_arquivo in os.listdir(dir):
-        with open(nome_arquivo, "r", encoding="utf-8") as f:
-            conteudo = f.read() 
-            for linha in conteudo:
+        path = os.path.join(dir, nome_arquivo)
+        # abre arquivos 1 a 1
+        with open(path, "r", encoding="utf-8") as f:
+            conteudo = f.read()
+            for linha in conteudo.splitlines():
                 if linha != "\n":
-                    linha = re.split(r"\W+", linha)
-                    for palavra in linha :
-                        palavra.lower()
-                        if palavra not in words.key():
-                            words = {palavra: dict()}
-                            aux = {palavra}
-                            aux = {nome_arquivo: 1}
-                        else:
-                            aux = words[palavra]
-                            aux[nome_arquivo] = (aux[nome_arquivo] + 1)
+                    # divide usando a regex (a-z A-Z 0-9 _)
+                    # linha = re.split(r"\W+", linha)
+                    linha = linha.split(" ")
+                    for palavra in linha:
+                        palavra = palavra.lower()
+                        if (
+                            ("-" or "_" not in palavra)
+                            and (len(palavra) > 2)
+                            and (palavra != "não")
+                        ):
+                            if palavra not in words.keys():
+                                # cria add palavra como chave do dict e um subdicionario como resultado, onde este tem nome do arquivo: frequencia
+                                words[palavra] = {nome_arquivo: 1}
+                            else:
+                                # add 1 na frequencia daquela palavra naquele arquivo
+                                words[palavra][nome_arquivo] += 1
 
-    
-    
+    save = open("index.txt", "w")
+    save.write("{}".format(words))
+    save.close()
+
+
+if __name__ == "__main__":
+    indexacao()
