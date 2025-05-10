0a2b6936bb43bb478d780946741a5cb6c7807a7b
Ivan Marcelino <ivanmarcelino2004@gmail.com>
2025-05-10 15:15:29 -0300

    Adicionando biblioteca lark para resolução de busca logica, adição de gramatica usada e classe para a busca

--- projeto.py
+++ projeto.py
@@ -2,7 +2,7 @@ import re
 import os
 import xml.etree.ElementTree as ET
 from collections import defaultdict, Counter
-import ttg
+from lark import Lark, Transformer
 
 
 # cria a arvore xml como: palavras -> documentos -> quantia da palavra
@@ -17,13 +17,16 @@ def saveIndexacaoXML(index: dict, path: str):
     tree = ET.ElementTree(root)
     tree.write(path, encoding="utf-8", xml_declaration=True)
 
+
 def saveIndexacaoBoolXML(index: dict, path: str):
     root = ET.Element("indexTerms")
     for palavra in index:
         word = ET.SubElement(root, "palavra", value=palavra)
         for doc in index[palavra]:
             documento = ET.SubElement(word, "documento", value=doc)
-            ET.SubElement(documento, "ocorrencia", value=str(1 if index[palavra][doc] else 0))
+            ET.SubElement(
+                documento, "ocorrencia", value=str(1 if index[palavra][doc] else 0)
+            )
 
     tree = ET.ElementTree(root)
     tree.write(path, encoding="utf-8", xml_declaration=True)
@@ -81,7 +84,9 @@ def indexacao(value_filter: int):
 
     # filtra os 50 mais frequentes palavras do dicionario:
     words = dict(
-        sorted(words.items(), key=lambda item: sum(item[1].values()), reverse=True)[:value_filter]
+        sorted(words.items(), key=lambda item: sum(item[1].values()), reverse=True)[
+            :value_filter
+        ]
     )
 
     # seta como 0 o index da palavra que não está presente naquele documento
@@ -109,6 +114,7 @@ def imprime_vocabulario(words: dict):
         count += 1
     print()
 
+
 def imprime_matriz_ocorrencia(words: dict):
     for palavra, documentos in words.items():
         print(f"\nPalavra: {palavra}")
@@ -125,12 +131,64 @@ def imprime_matriz_frequencia(words: dict):
         for doc, count in documentos.items():
             print(f"  - {doc}: {count} ocorrências")
 
+
 def contar_operadores(expressao):
-    operadores = [' and ', ' or ']
+    operadores = [" and ", " or "]
     total = 0
     for op in operadores:
         total += expressao.lower().count(op)
     return total
+
+
+class BooleanSearch(Transformer):
+    def __init__(self, index, all_docs):
+        self.index = index
+        self.all_docs = all_docs
+
+    def term(self, args):
+        word = args[0].value.lower()
+        return self.index.get(word, set())
+
+    def and_(self, args):
+        return args[0] & args[1]
+
+    def or_(self, args):
+        return args[0] | args[1]
+
+    def not_(self, args):
+        return self.all_docs - args[0]
+
+
+def busca_logica(index: dict, query: str):
+    # definindo a gramatica a ser usada
+    grammar = """
+    ?start: expr
+    ?expr: expr "OR" term     -> or_
+        | term
+    ?term: term "AND" factor  -> and_
+        | factor
+    ?factor: "NOT" factor     -> not_
+        | "(" expr ")"
+        | WORD             -> term
+
+    %import common.WORD
+    %ignore " "
+    """
+
+    # diicionario que vincula as palavras aos documentos somente
+    presence = {
+        # pega as palavras chave do dicionario
+        term: set(doc_freq.keys())
+        for term, doc_freq in index.items()
+    }
+
+    # itera por todos os valores do dicionario tentando adicionar o arquivo ao set de arquivos
+    all_docs = set()
+    for doc_freq in index.values():
+        all_docs.update(doc_freq)
+
+    parser = Lark(grammar, parser="lalr", transformer=BooleanSearch(presence, all_docs))
+    return parser.parse(query)
 
 
 if __name__ == "__main__":
@@ -164,19 +222,25 @@ if __name__ == "__main__":
             if words_filtered:
                 imprime_vocabulario(words_filtered)
             else:
-                print("* Arquivos ainda não foram indexados!                            *")
+                print(
+                    "* Arquivos ainda não foram indexados!                            *"
+                )
 
         elif opcao == "3":
             if words_filtered:
                 imprime_matriz_ocorrencia(words_filtered)
             else:
-                print("* Arquivos ainda não foram indexados!                            *")
+                print(
+                    "* Arquivos ainda não foram indexados!                            *"
+                )
 
         elif opcao == "4":
             if words_filtered:
                 imprime_matriz_frequencia(words_filtered)
             else:
-                print("* Arquivos ainda não foram indexados!                            *")
+                print(
+                    "* Arquivos ainda não foram indexados!                            *"
+                )
 
         elif opcao == "5":
             words_filtered = readIndexacaoXML("indexacao_freq.xml")
@@ -184,47 +248,81 @@ if __name__ == "__main__":
         elif opcao == "6":
             if words_filtered:
                 print("\n" + "*" * 70)
-                print("* CAMPO DE CONSULTA                                              *")
+                print(
+                    "* CAMPO DE CONSULTA                                              *"
+                )
                 print("*" * 70)
-                print("* Você pode realizar consultas utilizando termos e operadores    *")
-                print("* booleanos. Os operadores permitidos são:                       *")
-                print("*                                                                *")
-                print("*   - AND: para buscar documentos que contenham ambos os termos  *")
-                print("*   - OR : para buscar documentos que contenham pelo menos um    *")
-                print("*          dos termos                                            *")
-                print("*   - NOT: para buscar documentos que não contenham o termo      *")
-                print("*          posterior a ele                                       *")
-                print("*                                                                *")
-                print("* Exemplo de consultas válidas:                                  *")
-                print("*   gato AND cachorro                                            *")
-                print("*   carro OR moto                                                *")
-                print("*   livro AND autor OR editora                                   *")
-                print("*   NOT planta AND NOT animal                                    *")
-                print("*                                                                *")
-                print("* ATENÇÃO: Só é permitida a combinação de no máximo dois         *")
-                print("* operadores booleanos por consulta.                             *")
+                print(
+                    "* Você pode realizar consultas utilizando termos e operadores    *"
+                )
+                print(
+                    "* booleanos. Os operadores permitidos são:                       *"
+                )
+                print(
+                    "*                                                                *"
+                )
+                print(
+                    "*   - AND: para buscar documentos que contenham ambos os termos  *"
+                )
+                print(
+                    "*   - OR : para buscar documentos que contenham pelo menos um    *"
+                )
+                print(
+                    "*          dos termos                                            *"
+                )
+                print(
+                    "*   - NOT: para buscar documentos que não contenham o termo      *"
+                )
+                print(
+                    "*          posterior a ele                                       *"
+                )
+                print(
+                    "*                                                                *"
+                )
+                print(
+                    "* Exemplo de consultas válidas:                                  *"
+                )
+                print(
+                    "*   gato AND cachorro                                            *"
+                )
+                print(
+                    "*   carro OR moto                                                *"
+                )
+                print(
+                    "*   livro AND autor OR editora                                   *"
+                )
+                print(
+                    "*   NOT planta AND NOT animal                                    *"
+                )
+                print(
+                    "*                                                                *"
+                )
+                print(
+                    "* ATENÇÃO: Só é permitida a combinação de no máximo dois         *"
+                )
+                print(
+                    "* operadores booleanos por consulta.                             *"
+                )
                 print("*" * 70)
 
-                expressao =  input("\nDIGITE A EXPRESSÃO: ")
+                expressao = input("\nDIGITE A EXPRESSÃO: ")
 
                 # Verifica se há no máximo 2 operadores 'and'/'or'
                 if contar_operadores(expressao) > 2:
-                    print("\nErro: A expressão deve conter no máximo **dois operadores booleanos** (and/or).")
+                    print(
+                        "\nErro: A expressão deve conter no máximo **dois operadores booleanos** (and/or)."
+                    )
 
-
-                # Gera a tabela verdade
-                try:
-                    tabela = Truths(variables=[v.strip() for v in variaveis], expressions=[expressao])
-                    print("\nTabela Verdade:")
-                    print(tabela)
-                except Exception as e:
-                    print("\nErro ao gerar a tabela verdade:")
-                    print(f"Detalhes: {e}")
-
-
-
+                # realiza a busca busca_logica
+                result = busca_logica(words_filtered, expressao)
+                if result:
+                    print(f"{expressao} -> {result}")
+                else:
+                    print(f"{expressao} -> Nenhum documento que satisfaça a busca!")
             else:
-                print("* Arquivos ainda não foram indexados!                            *")
+                print(
+                    "* Arquivos ainda não foram indexados!                            *"
+                )
 
         elif opcao == "0":
             break
